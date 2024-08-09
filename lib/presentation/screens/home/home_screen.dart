54faa0da1a17d09d956fa23e1379cba4544c1f53
mab08 <frankyvargas15@gmail.com>
2024-08-09 15:48:18 -0500

    actualización home screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -12,6 +13,49 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+
+        final items = appMenuItems[index];
+        return _CustomListTile(items: items);
+
+      }
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.items,
+  });
+
+  final MenuItems items;
+
+  @override
+  Widget build(BuildContext context) {
+
+    // final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(items.icon, color: Colors.teal),
+      trailing: const Icon( Icons.arrow_forward_ios_rounded, color: Colors.teal,),
+      title: Text(items.title, style: const TextStyle(fontWeight: FontWeight.bold),),
+      subtitle: Text(items.subtitle),
+      onTap: () {
+
+      },
     );
   }
 }
